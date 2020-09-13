79743a73cb7f68738701924acd0a2327a92a3fb9
cravle <vladoxer280502@gmail.com>
2020-09-13 15:06:38 +0300

    fetch request

--- server.php
+++ server.php
@@ -1,5 +1,5 @@
 <?php
-$_POST = json_decode(file_get_contents("php://input"), true);
+$_POST = json_decode(file_get_contents("php://input"), true); // перевод json для работыв в php
 echo var_dump($_POST);
 
 ?>
